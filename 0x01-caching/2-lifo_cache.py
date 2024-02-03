a2bf1843dffc695aa19e08e2452b524bec9d07f5
Enoch Taylor <tay121222@gmail.com>
2024-02-03 05:02:19 +0000

    Fix LIFO - 1

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,6 +10,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialization"""
         super().__init__()
+        self.templist = []
 
     def put(self, key, item):
         """Put item into cache"""
@@ -17,11 +18,12 @@ class LIFOCache(BaseCaching):
             return
 
         if len(self.cache_data) >= self.MAX_ITEMS:
-            first_item = next(iter(self.cache_data))
+            first_item = self.templist.pop(0)
             print("DISCARD:", first_item)
             del self.cache_data[first_item]
 
         self.cache_data[key] = item
+        self.templist.append(key)
 
     def get(self, key):
         """Get item from cache"""

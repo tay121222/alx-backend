134ee183149fc141e1ad1c205424aa40c9fad59f
Enoch Taylor <tay121222@gmail.com>
2024-02-03 06:04:02 +0000

    Fixed MRU

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -9,21 +9,30 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """Initialization"""
         super().__init__()
+        self.item_key = None
 
     def put(self, key, item):
         """Add item to cache"""
         if key is None or item is None:
             return
 
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.item_key = key
+            return
+
         if len(self.cache_data) >= self.MAX_ITEMS:
-            item_key = next(reversed(self.cache_data))
-            print("DISCARD:", item_key)
-            del self.cache_data[item_key]
+            discarded_key = self.item_key
+            print("DISCARD:", discarded_key)
+            del self.cache_data[discarded_key]
 
         self.cache_data[key] = item
+        self.item_key = key
 
     def get(self, key):
         """Get item from cache"""
         if key is None or key not in self.cache_data:
             return None
+
+        self.item_key = key
         return self.cache_data[key]

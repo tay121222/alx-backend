f3574f6470bb087fbe5e394f8b3db440e64f071c
Enoch Taylor <tay121222@gmail.com>
2024-02-03 04:10:10 +0000

    class FIFOCache that inherits from BaseCaching and is a caching system - put performs FIFO operation. Get remains same

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""class FIFOCache"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """class FIFOCache that inherits from BaseCaching and is a caching system"""
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Put item into cache"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            first_item = next(iter(self.cache_data))
+            print("DISCARD:", first_item)
+            del self.cache_data[first_item]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item from cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

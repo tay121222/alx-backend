740b0c469b17a315f5986dfe4a49f1fd7005c0f7
Enoch Taylor <tay121222@gmail.com>
2024-03-06 20:52:40 +0000

    Writing the test for job creation

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,38 @@
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+import { expect } from 'chai';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  beforeEach(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+    queue.testMode.exit();
+  });
+
+  it('display a error message if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs(null, queue)).to.throw('Jobs is not an array');
+  });
+
+  it('create two new jobs to the queue', () => {
+    const jobs = [
+      { phoneNumber: '0542321532', message: 'OTP Code 56234' },
+      { phoneNumber: '0987654321', message: 'OTP Code 12323' }
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.deep.equal(jobs[0]);
+
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[1].data).to.deep.equal(jobs[1]);
+  });
+});
